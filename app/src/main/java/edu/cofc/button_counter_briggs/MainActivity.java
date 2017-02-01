d38bc963ecfb573cb41200239764cbd4145ae019
Kaelin Briggs <briggskp@g.cofc.edu>
2017-02-01 18:12:59 -0500

    Set button listeners

--- app/src/main/java/edu/cofc/button_counter_briggs/MainActivity.java
+++ app/src/main/java/edu/cofc/button_counter_briggs/MainActivity.java
@@ -2,6 +2,7 @@ package edu.cofc.button_counter_briggs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addButton = (Button) findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Nothing yet...
+            }
+        });
+
         subtractButton = (Button) findViewById(R.id.subtract_button);
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Nothing yet...
+            }
+        });
+
         clearButton = (Button) findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Nothing yet...
+            }
+        });
     }
 }
