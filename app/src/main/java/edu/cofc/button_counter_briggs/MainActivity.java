2211ccce1b3a19df07a5fbfd0abebae24539bea3
Kaelin Briggs <briggskp@g.cofc.edu>
2017-02-01 18:03:08 -0500

    Added Layout and String resources

--- app/src/main/java/edu/cofc/button_counter_briggs/MainActivity.java
+++ app/src/main/java/edu/cofc/button_counter_briggs/MainActivity.java
@@ -2,12 +2,20 @@ package edu.cofc.button_counter_briggs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button addButton;
+    private Button subtractButton;
+    private Button clearButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.add_button);
+        subtractButton = (Button) findViewById(R.id.subtract_button);
+        clearButton = (Button) findViewById(R.id.clear_button);
     }
 }
